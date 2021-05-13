cb29aaa895c1d78c3eab6bd2fd4f1a2b1ad642e8
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-05-13 11:51:41 +0200

    refactor test using a fixture file

--- test/main.test.js
+++ test/main.test.js
@@ -1,39 +1,9 @@
 const { ethers, upgrades } = require('hardhat');
 const { expect } = require('chai');
-
-async function deploy(name, ...params) {
-  const Contract = await ethers.getContractFactory(name);
-  return await Contract.deploy(...params).then(f => f.deployed());
-}
-
-async function deployUpgradeable(name, kind, ...params) {
-  const Contract = await ethers.getContractFactory(name);
-  return await upgrades.deployProxy(Contract, params, { kind }).then(f => f.deployed());
-}
+const { prepare, attach, deploy, deployUpgradeable } = require('./fixture');
 
 describe('Fortify', function () {
-  before(async function() {
-    this.accounts = await ethers.getSigners();
-    this.accounts.upgrader     = this.accounts.shift();
-    this.accounts.minter       = this.accounts.shift();
-    this.accounts.whitelister  = this.accounts.shift();
-    this.accounts.whitelist    = this.accounts.shift();
-    this.accounts.nonwhitelist = this.accounts.shift();
-    this.accounts.other        = this.accounts.shift();
-  });
-
-  beforeEach(async function () {
-    this.token = await deployUpgradeable('Fortify', 'uups', this.accounts.upgrader.address);
-    this.roles = {
-      UPGRADER:    await this.token.UPGRADER_ROLE(),
-      MINTER:      await this.token.MINTER_ROLE(),
-      WHITELISTER: await this.token.WHITELISTER_ROLE(),
-      WHITELIST:   await this.token.WHITELIST_ROLE(),
-    }
-    await this.token.connect(this.accounts.upgrader).grantRole(this.roles.MINTER, this.accounts.minter.address);
-    await this.token.connect(this.accounts.upgrader).grantRole(this.roles.WHITELISTER, this.accounts.whitelister.address);
-    await this.token.connect(this.accounts.whitelister).grantRole(this.roles.WHITELIST, this.accounts.whitelist.address);
-  });
+  prepare();
 
   it('check deployment', async function () {
     expect(await this.token.hasRole(this.roles.UPGRADER,    this.accounts.upgrader.address));
