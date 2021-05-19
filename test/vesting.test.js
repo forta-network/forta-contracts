73c5c3815883abb4a4ddc7b639231764d2750946
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-05-19 14:52:17 +0200

    expand vesting wallet tests

--- test/vesting.test.js
+++ test/vesting.test.js
@@ -2,9 +2,19 @@ const { ethers, upgrades } = require('hardhat');
 const { expect } = require('chai');
 const { prepare, deployUpgradeable } = require('./fixture');
 
+const min = (...args) => args.slice(1).reduce((x,y) => x.lt(y) ? x : y, args[0]);
+const max = (...args) => args.slice(1).reduce((x,y) => x.gt(y) ? x : y, args[0]);
 
-const start    = Date.now() / 1000 | 0;
+const start    = Date.now() / 1000 | 0 + 3600; // in 1 hour
 const duration = 4 * 365 * 86400; // 4 years
+const amount   = ethers.utils.parseEther('100');
+
+const schedule = Array(256).fill()
+  .map((_,i) => ethers.BigNumber.from(i).mul(duration).div(224).add(start))
+  .map(timestamp => ({
+      timestamp,
+      vested: min(amount.mul(timestamp.sub(start)).div(duration), amount)
+  }));
 
 describe('Fortify', function () {
   prepare();
@@ -12,7 +22,8 @@ describe('Fortify', function () {
   beforeEach(async function () {
     this.vesting = await deployUpgradeable('VestingWallet', 'uups', this.accounts.other.address, this.accounts.upgrader.address, start, duration);
     await this.token.connect(this.accounts.whitelister).grantRole(this.roles.WHITELIST, this.vesting.address);
-    await this.token.connect(this.accounts.minter).mint(this.vesting.address, 1);
+    await this.token.connect(this.accounts.whitelister).grantRole(this.roles.WHITELIST, this.accounts.other.address);
+    await this.token.connect(this.accounts.minter).mint(this.vesting.address, amount);
   });
 
   it('create vesting contract', async function () {
@@ -23,7 +34,29 @@ describe('Fortify', function () {
   });
 
   describe('vesting schedule', function () {
-    // TODO
+    it('check vesting schedule', async function () {
+      for (const { timestamp, vested } of schedule) {
+        expect(await this.vesting.vestedAmount(this.token.address, timestamp)).to.be.equal(vested);
+      }
+    });
+
+    it('execute vesting schedule', async function () {
+      let released = ethers.constants.Zero;
+
+      for (const { timestamp, vested } of schedule) {
+        await ethers.provider.send('evm_setNextBlockTimestamp', [ timestamp.toNumber() ]);
+        if (vested.gt(released)) {
+          await expect(this.vesting.release(this.token.address))
+            .to.emit(this.vesting, 'TokensReleased')
+            .withArgs(this.token.address, vested.sub(released))
+            .to.emit(this.token, 'Transfer')
+            .withArgs(this.vesting.address, this.accounts.other.address, vested.sub(released));
+          released = vested;
+        }
+        expect(await this.token.balanceOf(this.vesting.address)).to.be.equal(amount.sub(vested));
+        expect(await this.token.balanceOf(this.accounts.other.address)).to.be.equal(vested);
+      }
+    });
   });
 
   describe('delegate vote', function () {
@@ -43,7 +76,7 @@ describe('Fortify', function () {
         .to.emit(this.token, 'DelegateChanged')
         .withArgs(this.vesting.address, ethers.constants.AddressZero, this.accounts.other.address)
         .to.emit(this.token, 'DelegateVotesChanged')
-        .withArgs(this.accounts.other.address, 0, 1);
+        .withArgs(this.accounts.other.address, 0, amount);
 
       expect(await this.token.delegates(this.vesting.address)).to.be.equal(this.accounts.other.address);
     });
