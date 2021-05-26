1fd405ce588086b03de0e2cab0a4a9416cbad834
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-05-26 15:11:33 +0200

    clean IFortify.sol

--- contracts/IFortify.sol
+++ contracts/IFortify.sol
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// import "@openzeppelin/contracts/contracts/access/AccessControl.sol";
-// import "@openzeppelin/contracts/contracts/token/ERC20/extensions/draft-IERC20Votes.sol";
-// interface IFortify is IAccessControl, IERC20Votes {}
-
 import "@openzeppelin/contracts-upgradeable/contracts/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/contracts/token/ERC20/extensions/draft-ERC20VotesUpgradeable.sol";
 
