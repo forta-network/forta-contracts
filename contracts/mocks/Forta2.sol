8def347233c7e909c37666548c3082ff42cd2a4b
Francisco Giordano <frangio.1@gmail.com>
2021-10-28 23:25:49 -0300

    add FortaBridged contract

--- contracts/mocks/Forta2.sol
+++ contracts/mocks/Forta2.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../Forta.sol";
+import "../token/Forta.sol";
 
 contract Forta2 is Forta {
     function version() external pure returns (string memory) {
---
+++ contracts/token/FortaBridged.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./FortaCommon.sol";
+
+contract FortaBridged is FortaCommon {
+    address private childChainManagerProxy;
+
+    function initialize(address admin, address _childChainManagerProxy) public initializer {
+        __FortaCommon_init(admin);
+
+        childChainManagerProxy = _childChainManagerProxy;
+    }
+
+    function deposit(address user, bytes calldata depositData) external {
+        require(msg.sender == childChainManagerProxy, "FortaBridged: only childChainManagerProxy can deposit");
+        uint256 amount = abi.decode(depositData, (uint256));
+        _mint(user, amount);
+    }
+
+    function withdraw(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function updateChildChainManager(address _childChainManagerProxy) external onlyRole(ADMIN_ROLE) {
+        require(_childChainManagerProxy != address(0), "FortaBridged: bad childChainManagerProxy address");
+        childChainManagerProxy = _childChainManagerProxy;
+    }
+}
