d67edd502f747047abe486bd2a368747325ea7d3
Raul <raul.mf86@gmail.com>
2021-12-17 12:41:23 +0100

    use ENS from package

--- contracts/tools/ENSReverseRegistration.sol
+++ contracts/tools/ENSReverseRegistration.sol
@@ -1,30 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IENS {
-    event NewOwner(bytes32 indexed node, bytes32 indexed label, address owner);
-    event Transfer(bytes32 indexed node, address owner);
-    event NewResolver(bytes32 indexed node, address resolver);
-    event NewTTL(bytes32 indexed node, uint64 ttl);
-    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
-
-    function setRecord(bytes32, address, address, uint64) external;
-    function setSubnodeRecord(bytes32, bytes32, address, address, uint64) external;
-    function setSubnodeOwner(bytes32, bytes32, address) external returns(bytes32);
-    function setResolver(bytes32, address) external;
-    function setOwner(bytes32, address) external;
-    function setTTL(bytes32, uint64) external;
-    function setApprovalForAll(address, bool) external;
-    function owner(bytes32) external view returns (address);
-    function resolver(bytes32) external view returns (address);
-    function ttl(bytes32) external view returns (uint64);
-    function recordExists(bytes32) external view returns (bool);
-    function isApprovedForAll(address, address) external view returns (bool);
-}
+import { ENS } from "@ensdomains/ens-contracts/contracts/registry/ENS.sol";
 
 interface IReverseRegistrar {
     function ADDR_REVERSE_NODE() external view returns (bytes32);
-    function ens() external view returns (IENS);
+    function ens() external view returns (ENS);
     function defaultResolver() external view returns (address);
     function claim(address) external returns (bytes32);
     function claimWithResolver(address, address) external returns (bytes32);
@@ -37,6 +18,6 @@ library ENSReverseRegistration {
     bytes32 internal constant ADDR_REVERSE_NODE = 0x91d1777781884d03a6757a803996e38de2a42967fb37eeaca72729271025a9e2;
 
     function setName(address ensregistry, string calldata ensname) internal {
-        IReverseRegistrar(IENS(ensregistry).owner(ADDR_REVERSE_NODE)).setName(ensname);
+        IReverseRegistrar(ENS(ensregistry).owner(ADDR_REVERSE_NODE)).setName(ensname);
     }
 }
