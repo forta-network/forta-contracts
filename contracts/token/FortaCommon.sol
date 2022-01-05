fa254824daa4216acd54eb5ff407c1d59ad537c7
Raul <raul.mf86@gmail.com>
2022-01-05 12:58:41 +0100

    reintroduce n06 fix

--- contracts/token/FortaCommon.sol
+++ contracts/token/FortaCommon.sol
@@ -24,7 +24,6 @@ contract FortaCommon is AccessControlUpgradeable, ERC20VotesUpgradeable, UUPSUpg
         _setRoleAdmin(WHITELISTER_ROLE, ADMIN_ROLE);
         _setRoleAdmin(WHITELIST_ROLE, WHITELISTER_ROLE);
         _grantRole(ADMIN_ROLE, admin);
-        _grantRole(ADMIN_ROLE, address(this)); // required by grantWhitelister
     }
 
     // Allow whitelister to assign other whitelisters
