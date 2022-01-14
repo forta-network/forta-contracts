949b87151e54e82c214955ae37d92dac07cc35c0
Raul <raul.mf86@gmail.com>
2022-01-14 19:27:07 +0100

    deployed upgrades for Scanners and Dispatch on goerli

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -2,44 +2,6 @@ const { ethers, upgrades } = require('hardhat');
 const DEBUG                = require('debug')('forta');
 const utils                = require('./utils');
 
-//upgrades.silenceWarnings();
-
-const upgradesCache = {
-
-    "agents": {
-        "0.1.1": {
-            "contract": "AgentRegistry_0_1_1"
-        },
-        "0.1.2": {
-            "contract": "AgentRegistry",
-        }
-    }
-    
-    
-}
-
-async function upgradeImpl(chainId, key, version, call = undefined, constructorArgs = [], unsafeAllow = []) {
-    console.log(chainId, key, version)
-    const allParams = upgradesCache[key][version]
-    console.log(allParams)
-    console.log(call)
-    console.log(constructorArgs)
-    console.log(unsafeAllow)
-
-    const CACHE = new utils.AsyncConf({ cwd: __dirname, configName: `.cache-${chainId}` });
-    console.log(key)
-    const NewImplementation = await ethers.getContractFactory(allParams.contract);
-    return upgrades.upgradeProxy(
-        await CACHE.get(key),
-        NewImplementation,
-        {
-            call: call,
-            constructorArgs: constructorArgs,
-            unsafeAllow: unsafeAllow
-        }
-    );
-
-} 
 
 async function main() {
     const provider = config?.provider ?? config?.deployer?.provider ?? await utils.getDefaultProvider();
@@ -70,7 +32,7 @@ async function main() {
         await contracts.access.grantRole(UPGRADER_ROLE,      deployer.address        ).then(tx => tx.wait());
         console.log('Granted upgrader role to: ', deployer.address)
     }
-    /*
+    
     const AgentRegistry = await ethers.getContractFactory('AgentRegistry');
     const newAgentRegistry = await upgrades.upgradeProxy(
         contracts.agents.address,
@@ -86,7 +48,7 @@ async function main() {
         }
     );
     console.log('new Agent Registry: ', await upgrades.erc1967.getImplementationAddress(newAgentRegistry.address))
-    */
+    
     
     const ScannerRegistry = await ethers.getContractFactory('ScannerRegistry');
     const newScannerRegistry = await upgrades.upgradeProxy(
@@ -104,10 +66,10 @@ async function main() {
     );
     
     console.log('newScannerRegistry: ', await upgrades.erc1967.getImplementationAddress(newScannerRegistry.address))
-        /*
+    
     const Dispatch = await ethers.getContractFactory('Dispatch');
     const newDispatch = await upgrades.upgradeProxy(
-        dispatch.address,
+        contracts.dispatch.address,
         Dispatch,
         {
 
@@ -118,21 +80,8 @@ async function main() {
     );
 
     console.log('newDispatch: ', await upgrades.erc1967.getImplementationAddress(newDispatch.address))
-    */
-/*
-    const contracts = await Promise.all(Object.entries({
-        access:    utils.attach('AccessManager',   'access.forta.eth'             ).then(contract => contract.connect(deployer)),
-        forwarder: utils.attach('Forwarder',       'forwarder.forta.eth'          ).then(contract => contract.connect(deployer)),
-    }).map(entry => Promise.all(entry))).then(Object.fromEntries);
+    
 
-    const roles = await Promise.all(Object.entries({
-
-        REWARDS_ADMIN: ethers.utils.id('REWARDS_ADMIN_ROLE'), 
-    }).map(entry => Promise.all(entry))).then(Object.fromEntries);
-
-    // await contracts.access.grantRole(roles.UPGRADER,      deployer.address        ).then(tx => tx.wait());
-   */
-    //upgrade()
 }
 
 main()
