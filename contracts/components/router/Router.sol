1f19717cc711522afb05299c8c3e05238484de0d
Raul <raul.mf86@gmail.com>
2022-02-16 20:00:50 +0100

    option for strict hooks

--- contracts/components/router/Router.sol
+++ contracts/components/router/Router.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/Multicall.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
@@ -10,15 +10,16 @@ import "../utils/ForwardedContext.sol";
 import "../../tools/ENSReverseRegistration.sol";
 import "./IRouter.sol";
 
-// This should be BaseComponentUpgradeable, because BaseComponentUpgradeable is Routed
+// This should be BaseComponent, because BaseComponent is Routed
 contract Router is IRouter, ForwardedContext, AccessManagedUpgradeable, UUPSUpgradeable, Multicall {
     using EnumerableSet for EnumerableSet.AddressSet;
 
     mapping(bytes4 => EnumerableSet.AddressSet) private _routingTable;
+    mapping(bytes4 => bool) private _revertsOnFail;
 
     string public constant version = "0.1.0";
-    
-    event RoutingUpdated(bytes4 indexed sig, address indexed target, bool enable);
+
+    event RoutingUpdated(bytes4 indexed sig, address indexed target, bool enable, bool revertsOnFail);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor(address forwarder) initializer ForwardedContext(forwarder) {}
@@ -32,20 +33,24 @@ contract Router is IRouter, ForwardedContext, AccessManagedUpgradeable, UUPSUpgr
         bytes4 sig = bytes4(payload[:4]);
         uint256 length = _routingTable[sig].length();
         for (uint256 i = 0; i < length; ++i) {
-            // Lazy, don't worry about calls failing here
             (bool success, bytes memory returndata) = _routingTable[sig].at(i).call(payload);
+            if (_revertsOnFail[sig]) {
+                require(success, "Router: hook failed");
+            }
             success;
             returndata;
         }
     }
 
-    function setRoutingTable(bytes4 sig, address target, bool enable) external onlyRole(ROUTER_ADMIN_ROLE) {
+    function setRoutingTable(bytes4 sig, address target, bool enable, bool revertsOnFail) external onlyRole(ROUTER_ADMIN_ROLE) {
         if (enable) {
             _routingTable[sig].add(target);
+            _revertsOnFail[sig] = revertsOnFail;
         } else {
             _routingTable[sig].remove(target);
+            _revertsOnFail[sig] = false;
         }
-        emit RoutingUpdated(sig, target, enable);
+        emit RoutingUpdated(sig, target, enable, revertsOnFail);
     }
 
 
@@ -66,5 +71,5 @@ contract Router is IRouter, ForwardedContext, AccessManagedUpgradeable, UUPSUpgr
         return super._msgData();
     }
 
-    uint256[49] private __gap;
+    uint256[48] private __gap;
 }
