20dd03305f8522373ded9d2f5807089ab23e61f7
Raúl <raul.mf86@gmail.com>
2022-02-23 12:56:41 +0100

    separated contracts (#69)

--- contracts/components/metatx/Forwarder.sol
+++ contracts/components/metatx/Forwarder.sol
@@ -2,37 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
-
-abstract contract EIP712WithNonce is EIP712 {
-    event NonceUsed(address indexed user, uint256 indexed timeline, uint256 nonce);
-
-    mapping(address => mapping(uint256 => uint256)) private _nonces;
-
-    function DOMAIN_SEPARATOR() external view returns (bytes32) {
-        return _domainSeparatorV4();
-    }
-
-    function getNonce(address from) public view virtual returns (uint256) {
-        return _nonces[from][0];
-    }
-
-    function getNonce(address from, uint256 timeline) public view virtual returns (uint256) {
-        return _nonces[from][timeline];
-    }
-
-    function _verifyAndConsumeNonce(address user, uint256 fullNonce) internal virtual {
-        uint256 timeline = fullNonce >> 128;
-        uint256 nonce    = uint128(fullNonce);
-        uint256 expected = _nonces[user][timeline]++;
-
-        require(nonce == expected, "EIP712WithNonce: invalid-nonce");
-
-        emit NonceUsed(user, timeline, nonce);
-    }
-}
-
 import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import "./EIP712WithNonce.sol";
 
 contract Forwarder is EIP712WithNonce {
     struct ForwardRequest {
