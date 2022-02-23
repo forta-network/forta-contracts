70b6c454feb97502e0c4d0dceff9f3a526bf634b
Raul <raul.mf86@gmail.com>
2022-02-23 18:44:28 +0100

    separate interfaces

--- contracts/components/staking/IStakeController.sol
+++ contracts/components/staking/IStakeController.sol
@@ -1,18 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./IStakeSubject.sol";
+
 interface IStakeController {
     event StakeSubjectHandlerChanged(address newHandler, address oldHandler);
     function setStakeSubjectHandler(uint8 subjectType, IStakeSubject subjectHandler) external;
     function activeStakeFor(uint8 subjectType, uint256 subject) external view returns(uint256);
     function maxStakeFor(uint8 subjectType, uint256 subject) external view returns(uint256);
     function minStakeFor(uint8 subjectType, uint256 subject) external view returns(uint256);
-}
-interface IStakeSubject {
-    struct StakeThreshold {
-        uint256 min;
-        uint256 max;
-    }
-    function getStakeThreshold(uint256 subject) external view returns (StakeThreshold memory);
-    function isStakedOverMin(uint256 subject) external view returns (bool);
 }
