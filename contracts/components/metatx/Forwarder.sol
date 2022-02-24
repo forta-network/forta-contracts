5d76147fcadc328781c63a30f3e3c13a20e2ff53
Raúl <raul.mf86@gmail.com>
2022-02-24 22:00:57 +0100

    N03: Add explicit warning about the Forwarder (#61)
    * cancelling forwarding transactions and comment
    
    * added comments, removed cancelTransaction

--- contracts/components/metatx/Forwarder.sol
+++ contracts/components/metatx/Forwarder.sol
@@ -21,13 +21,16 @@ contract Forwarder is EIP712WithNonce {
     constructor() EIP712("Forwarder", "1") {}
 
     /**
-     * Executes a ForwardRequest (meta-tx) if signature is verified, deadline is met and nonce is valid
-     * NOTE: This implementations allows for out of order execution, by allowing several "timelines" per nonce
+     * @notice Executes a ForwardRequest (meta-tx) if signature is verified, deadline is met and nonce is valid
+     * @dev This implementations allows for out of order execution, by allowing several "timelines" per nonce
      * by splitting the uint256 type space into 128 bit subspaces where each subspace is interpreted as maintaining
      * an ordered timeline. The intent of the design is to allow multiple nonces to be valid at any given time.
      * For a detailed explanation: https://github.com/amxx/permit#out-of-order-execution
      * For an example on how to leverage this functionality, see tests/forwarder/forwarder.test.js
      * Will emit NonceUsed(user, timeline, nonce) for better reporting / UX 
+     * WARNING: failed transactions do not consume a nonce, unlinke regular ethereum transactions. Please make use
+     * of the deadline functionality, and if you want to cancel a request, submit a successful transaction with the same
+     * nonce. 
      * @param req  ForwardRequest to be executed
      * @param signature EIP-712 signature of the ForwardRequest
      * @return (success, returnData) of the executed request 
