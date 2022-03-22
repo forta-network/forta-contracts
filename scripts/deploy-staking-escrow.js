4f905d5094e35a9c8d56bc07fd860e1dd0452aa7
Raul <raul.mf86@gmail.com>
2022-03-22 15:20:23 +0100

    deploy vesting wallet and scrow scripts

--- scripts/deploy-staking-escrow.js
+++ scripts/deploy-staking-escrow.js
@@ -1,6 +1,7 @@
 const { ethers, upgrades, network } = require('hardhat');
 const DEBUG                = require('debug')('forta:migration');
 const utils                = require('./utils');
+const assert = require('assert');
 const SCANNER_SUBJECT = 0;
 const AGENT_SUBJECT = 1;
 const semver = require('semver')
@@ -13,9 +14,6 @@ const CHILD_CHAIN_MANAGER_PROXY = {
 };
 
 
-/*********************************************************************************************************************
- *                                                Migration workflow                                                 *
- *********************************************************************************************************************/
 async function main() {
     const provider = config?.provider ?? config?.deployer?.provider ?? await utils.getDefaultProvider();
     const deployer = config?.deployer ??                               await utils.getDefaultDeployer(provider);
@@ -26,10 +24,26 @@ async function main() {
     DEBUG(`Balance:  ${await provider.getBalance(deployer.address).then(ethers.utils.formatEther)}${ethers.constants.EtherSymbol}`);
     DEBUG('----------------------------------------------------');
     utils.assertNotUsingHardhatKeys(chainId, deployer);
+    
+    const CACHE_L2 = new utils.AsyncConf({ cwd: __dirname, configName: `.cache-${chainId}` });
+    let CACHE_L1
+    
+    switch (chainId) {
+        case 80001:
+            CACHE_L1 = new utils.AsyncConf({ cwd: __dirname, configName: `.cache-${5}` });
+            break;
+        case 137:
+            CACHE_L1 = new utils.AsyncConf({ cwd: __dirname, configName: `.cache-${1}` });
+            break;
+        default:
+            throw new Error(`Unsupported chain ${chainId}`);
 
-    const CACHE = new utils.AsyncConf({ cwd: __dirname, configName: `.cache-${chainId}` });
-
-    const contracts = {}
+    }
+    const manager = deployer.address;
+    const l2EscrowFactory = await utils.attach('StakingEscrowFactory', await CACHE_L2.get('escrow-factory.address'));
+    const l1Vesting = await CACHE_L1.get(`vesting-${manager}.address`)
+    console.log(await l2EscrowFactory.newWallet(l1Vesting, manager))
+    
 
 
 }
@@ -43,4 +57,4 @@ if (require.main === module) {
         });
 }
 
-module.exports = migrate;
+module.exports = main;
