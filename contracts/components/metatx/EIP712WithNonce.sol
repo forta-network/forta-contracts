0761b3abe79c29e3840cbffe8ed98aba31035e99
Raul <raul.mf86@gmail.com>
2022-04-12 15:06:34 +0200

    update license headers

--- contracts/components/metatx/EIP712WithNonce.sol
+++ contracts/components/metatx/EIP712WithNonce.sol
@@ -1,4 +1,6 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
+// See Forta Business Source License: https://github.com/forta-protocol/forta-contracts/blob/master/LICENSE.md
+
 
 pragma solidity ^0.8.4;
 
