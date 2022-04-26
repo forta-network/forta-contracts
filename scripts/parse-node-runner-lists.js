3d018eba98eb002c80f8e5fcb16dee4b557a1e96
Raul <raul.mf86@gmail.com>
2022-04-26 16:34:43 +0200

    node exports

--- scripts/parse-node-runner-lists.js
+++ scripts/parse-node-runner-lists.js
@@ -1,9 +1,21 @@
 const fs = require('fs');
-
-const funnel = require('./data/funnel2.json');
+const yesterdayNodes = require('./data/nodes-to-stake_1650914637350.json');
+const funnel = require('./data/funnel3.json');
 
 const nodes = Object.keys(funnel).map( key => funnel[key]['NODE address']).filter(x => x.startsWith('0x'));
 
-console.log('# of nodes:', nodes.length);
+const b = new Set(yesterdayNodes);
+const difference = nodes.filter(x => !b.has(x))
 
-fs.writeFileSync(`./scripts/data/2nodes-to-stake_${Date.now()}.json`, JSON.stringify(nodes))
+
+const result = {
+    'all-nodes-to-date': nodes,
+    'last-batch': yesterdayNodes,
+    'new-nodes': difference
+}
+
+console.log('# of nodes:', nodes.length);
+console.log('# of yesterda:', yesterdayNodes.length);
+console.log('# new owns:', difference.length);
+
+fs.writeFileSync(`./scripts/data/nodes-to-stake_difference_${Date.now()}.json`, JSON.stringify(result))
