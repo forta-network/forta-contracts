18314e269fe2e19d3afe81c97b92a8a27539c655
Raul <raul.mf86@gmail.com>
2022-04-29 23:32:38 +0200

    deployed dummy vesting, finished script

--- scripts/deploy-vesting-wallet.js
+++ scripts/deploy-vesting-wallet.js
@@ -13,13 +13,7 @@ const ROOT_CHAIN_MANAGER = {
     5:     '0xBbD7cBFA79faee899Eaf900F13C9065bF03B1A74',
 };
 
-const allocation = {
-    beneficiary: null,
-    upgrader: null,
-    start: '2022-03-21T00:00:00Z',
-    cliff: '30 minutes',
-    duration: '30 days',
-}
+const allocations = require('./CONFIG.js').allocations_29042020;
 
 
 async function main() {
@@ -30,18 +24,25 @@ async function main() {
     DEBUG(`ENS:      ${provider.network.ensAddress ?? 'undetected'}`);
     DEBUG(`Deployer: ${deployer.address}`);
     DEBUG(`Balance:  ${await provider.getBalance(deployer.address).then(ethers.utils.formatEther)}${ethers.constants.EtherSymbol}`);
+    DEBUG(allocations)
+    
     DEBUG('----------------------------------------------------');
     utils.assertNotUsingHardhatKeys(chainId, deployer);
     
     const CACHE_L1 = new utils.AsyncConf({ cwd: __dirname, configName: `.cache-${chainId}` });
     let CACHE_L2
-    const beneficiary = allocation.beneficiary ?? deployer.address;
-    const upgrader       = allocation.upgrader ?? deployer.address;
-    const start       = utils.dateToTimestamp(allocation.start);
-    const cliff       = utils.durationToSeconds(allocation.cliff);
-    const duration    = utils.durationToSeconds(allocation.duration);
+    const parameters = allocations.map(allocation => {
+        return {
+            beneficiary: allocation.beneficiary,
+            upgrader: allocation.upgrader,
+            start: utils.dateToTimestamp(allocation.start),
+            cliff: utils.durationToSeconds(allocation.cliff),
+            duration: utils.durationToSeconds(allocation.duration)
+        }
+    })
+    DEBUG(parameters)
     const l1Token = await CACHE_L1.get('forta.address');
-    
+
     let rootChainManager
     switch (chainId) {
         case 1:
@@ -56,72 +57,79 @@ async function main() {
             throw new Error(`Unsupported chain ${chainId}`);
 
     }
+    DEBUG('l1Token', l1Token)
+    DEBUG('rootChainManager', rootChainManager)
+
     const l2EscrowFactory = await CACHE_L2.get('escrow-factory.address');
     const l2EscrowTemplate = await CACHE_L2.get('escrow-template.address');
-    console.log(l2EscrowFactory, l2EscrowTemplate)
+    DEBUG('l2EscrowFactory', l2EscrowFactory)
+    DEBUG('l2EscrowTemplate', l2EscrowTemplate)
     
+    var index = 0;
     console.log('Deploying:')
-    //console.log(Object.entries(allocation))
-    Object.entries(allocation).forEach(x => console.log(x));
-         
+    for(const params of parameters) {
+        const vesting = await ethers.getContractFactory('VestingWalletV2', deployer).then(factory => utils.tryFetchProxy(
+            CACHE_L1,
+            `vesting-${params.beneficiary}`,
+            factory,
+            'uups',
+            [ params.beneficiary, params.upgrader, params.start, params.cliff, params.duration ],
+            {
+                constructorArgs: [
+                    rootChainManager,
+                    l1Token,
+                    l2EscrowFactory,
+                    l2EscrowTemplate,
+                ],
+                unsafeAllow: 'delegatecall' },
+        ));
+        /*
+        const vesting = await utils.performUpgrade(
+            await utils.attach('VestingWalletV2', await CACHE_L1.get(`vesting-${deployer.address}.address`)),
+            await ethers.getContractFactory('VestingWalletV2', deployer),
+            {
+                constructorArgs: [
+                    rootChainManager,
+                    l1Token,
+                    l2EscrowFactory,
+                    l2EscrowTemplate,
+                ],
+                unsafeAllow: 'delegatecall'
+            },
+            CACHE_L1,
+            `vesting-${deployer.address}`
+        );*/
     
-    /*const vesting = await ethers.getContractFactory('VestingWalletV2', deployer).then(factory => utils.tryFetchProxy(
-        CACHE_L1,
-        `vesting-${beneficiary}`,
-        factory,
-        'uups',
-        [ beneficiary, upgrader, start, cliff, duration ],
-        {
-            constructorArgs: [
-                rootChainManager,
-                l1Token,
-                l2EscrowFactory,
-                l2EscrowTemplate,
-            ],
-            unsafeAllow: 'delegatecall' },
-    ));*/
-    const vesting = await utils.performUpgrade(
-        await utils.attach('VestingWalletV2', await CACHE_L1.get(`vesting-${deployer.address}.address`)),
-        await ethers.getContractFactory('VestingWalletV2', deployer),
-        {
-            constructorArgs: [
-                rootChainManager,
-                l1Token,
-                l2EscrowFactory,
-                l2EscrowTemplate,
-            ],
-            unsafeAllow: 'delegatecall'
-        },
-        CACHE_L1,
-        `vesting-${deployer.address}`
-    );
-
-    console.log('Deployed vesting wallet:', vesting.address);
-    console.log('Post deployment checks...');
-
-    await Promise.all([
-        vesting.start(),
-        vesting.cliff(),
-        vesting.duration(),
-        vesting.beneficiary(),
-        vesting.owner(),
-        vesting.rootChainManager(),
-        vesting.l1Token(),
-        vesting.l2EscrowFactory(),
-        vesting.l2EscrowTemplate(),
-        vesting.historicalBalanceMin()
-      ]).then(([ start, cliff, duration, beneficiary, owner, rootChainManager, l1Token, l2EscrowFactory, l2EscrowTemplate, historicalBalanceMin]) => {
-        assert(start.eq(utils.dateToTimestamp(allocation.start)),         `Wrong start for vested allocation to ${beneficiary}`);
-        assert(cliff.eq(utils.durationToSeconds(allocation.cliff)),       `Wrong cliff for vested allocation to ${beneficiary}`);
-        assert(duration.eq(utils.durationToSeconds(allocation.duration)), `Wrong duration for vested allocation to ${beneficiary}`);
-        assert.strictEqual(beneficiary.toLowerCase(), beneficiary.toLowerCase(), `Wrong beneficiary for vested allocation to ${beneficiary}`);
-        assert.strictEqual(owner.toLowerCase(), upgrader.toLowerCase(),    `Wrong admin for direct allocation to ${beneficiary}`);
-        assert.strictEqual(rootChainManager.toLowerCase(), rootChainManager.toLowerCase(),  `Wrong rootChainManager for ${beneficiary}`);
-        assert.strictEqual(l1Token.toLowerCase(), l1Token.toLowerCase(),    `Wrong l1Token for direct allocation to ${l1Token}`);
-        assert.strictEqual(l2EscrowFactory.toLowerCase(), l2EscrowFactory.toLowerCase(),    `Wrong l2EscrowFactory to ${beneficiary}`);
-        assert.strictEqual(l2EscrowTemplate.toLowerCase(), l2EscrowTemplate.toLowerCase(),    `Wrong l2EscrowFactory to ${beneficiary}`);
+        console.log('Deployed vesting wallet:', vesting.address);
+        console.log('Post deployment checks...');
+    
+        const results = await Promise.all([
+            vesting.start(),
+            vesting.cliff(),
+            vesting.duration(),
+            vesting.beneficiary(),
+            vesting.owner(),
+            vesting.rootChainManager(),
+            vesting.l1Token(),
+            vesting.l2EscrowFactory(),
+            vesting.l2EscrowTemplate(),
+            vesting.historicalBalanceMin()
+        ])
+        const [ start, cliff, duration, beneficiary, owner, vRootChainManager, vL1Token, vL2EscrowFactory, vL2EscrowTemplate, historicalBalanceMin] = results;
+        assert(start.eq(utils.dateToTimestamp(allocations[index].start)),         `Wrong start for vested allocation to ${beneficiary}`);
+        assert(cliff.eq(utils.durationToSeconds(allocations[index].cliff)),       `Wrong cliff for vested allocation to ${beneficiary}`);
+        assert(duration.eq(utils.durationToSeconds(allocations[index].duration)), `Wrong duration for vested allocation to ${beneficiary}`);
+        assert.strictEqual(beneficiary.toLowerCase(), allocations[index].beneficiary.toLowerCase(), `Wrong beneficiary for vested allocation to ${beneficiary}`);
+        assert.strictEqual(owner.toLowerCase(), allocations[index].upgrader.toLowerCase(),    `Wrong admin for direct allocation to ${beneficiary}`);
+        assert.strictEqual(vRootChainManager.toLowerCase(), rootChainManager.toLowerCase(),  `Wrong rootChainManager for ${beneficiary}`);
+        assert.strictEqual(vL1Token.toLowerCase(), l1Token.toLowerCase(),    `Wrong l1Token for direct allocation to ${l1Token}`);
+        assert.strictEqual(vL2EscrowFactory.toLowerCase(), l2EscrowFactory.toLowerCase(),    `Wrong l2EscrowFactory to ${beneficiary}`);
+        assert.strictEqual(vL2EscrowTemplate.toLowerCase(), l2EscrowTemplate.toLowerCase(),    `Wrong l2EscrowFactory to ${beneficiary}`);
         assert(historicalBalanceMin.eq(ethers.BigNumber.from(0)),       `historicalBalanceMin != 0 ${beneficiary}`);
-      });
+        console.log('Checks!...');
+
+        index++;
+    }
 }
 
 if (require.main === module) {
