50ed56f7ab2183185199c12d9019121798d423cc
Raul <raul.mf86@gmail.com>
2022-05-02 18:22:26 +0200

    script to deploy wallet implementations

--- scripts/deploy-vesting-wallet-implentations.js
+++ scripts/deploy-vesting-wallet-implentations.js
@@ -1,7 +1,6 @@
 const { ethers, upgrades } = require('hardhat');
 const DEBUG                = require('debug')('forta:migration');
 const utils                = require('./utils');
-const assert = require('assert');
 
 
 upgrades.silenceWarnings();
@@ -11,7 +10,6 @@ const ROOT_CHAIN_MANAGER = {
     5:     '0xBbD7cBFA79faee899Eaf900F13C9065bF03B1A74',
 };
 
-const allocations = require('./CONFIG.js').allocations_29042020;
 
 const formatL1VestingWallets = async (cache, chainId) => {
     const l1JSON = require(`./.cache-${chainId}.json`)
@@ -26,6 +24,14 @@ const formatL1VestingWallets = async (cache, chainId) => {
     }
 }
 
+const getBeneficiaryList = (chainId) => {
+    const l1JSON = require(`./.cache-${chainId}.json`)
+    return Object.keys(l1JSON)
+        .filter(key => key.startsWith('vesting-') && !key.startsWith('vesting-dummy') && !key.endsWith('pending'))
+        .map(key => key.replace('vesting-', ''))
+    
+}
+
 
 async function main() {
     const provider = config?.provider ?? config?.deployer?.provider ?? await utils.getDefaultProvider();
@@ -35,27 +41,16 @@ async function main() {
     DEBUG(`ENS:      ${provider.network.ensAddress ?? 'undetected'}`);
     DEBUG(`Deployer: ${deployer.address}`);
     DEBUG(`Balance:  ${await provider.getBalance(deployer.address).then(ethers.utils.formatEther)}${ethers.constants.EtherSymbol}`);
-    DEBUG(allocations)
     
     DEBUG('----------------------------------------------------');
     utils.assertNotUsingHardhatKeys(chainId, deployer);
     
+    const vestingBeneficiaries = getBeneficiaryList(chainId);
     const CACHE_L1 = new utils.AsyncConf({ cwd: __dirname, configName: `.cache-${chainId}` });
-    await formatL1VestingWallets(CACHE_L1, chainId);
-    return
     
 
     let CACHE_L2
-    const parameters = allocations.map(allocation => {
-        return {
-            beneficiary: allocation.beneficiary,
-            upgrader: allocation.upgrader,
-            start: utils.dateToTimestamp(allocation.start),
-            cliff: utils.durationToSeconds(allocation.cliff),
-            duration: utils.durationToSeconds(allocation.duration)
-        }
-    })
-    DEBUG(parameters)
+    
     const l1Token = await CACHE_L1.get('forta.address');
 
     let rootChainManager
@@ -81,68 +76,13 @@ async function main() {
     DEBUG('l2EscrowTemplate', l2EscrowTemplate)
     
     var index = 0;
-    console.log('Deploying:')
-    for(const params of parameters) {
-        const vesting = await ethers.getContractFactory('VestingWalletV2', deployer).then(factory => utils.tryFetchProxy(
-            CACHE_L1,
-            `vesting-${params.beneficiary}`,
-            factory,
-            'uups',
-            [ params.beneficiary, params.upgrader, params.start, params.cliff, params.duration ],
-            {
-                constructorArgs: [
-                    rootChainManager,
-                    l1Token,
-                    l2EscrowFactory,
-                    l2EscrowTemplate,
-                ],
-                unsafeAllow: 'delegatecall' },
-        ));
-        /*
-        const vesting = await utils.performUpgrade(
-            await utils.attach('VestingWalletV2', await CACHE_L1.get(`vesting-${deployer.address}.address`)),
-            await ethers.getContractFactory('VestingWalletV2', deployer),
-            {
-                constructorArgs: [
-                    rootChainManager,
-                    l1Token,
-                    l2EscrowFactory,
-                    l2EscrowTemplate,
-                ],
-                unsafeAllow: 'delegatecall'
-            },
-            CACHE_L1,
-            `vesting-${deployer.address}`
-        );*/
-    
-        console.log('Deployed vesting wallet:', vesting.address);
-        console.log('Post deployment checks...');
-    
-        const results = await Promise.all([
-            vesting.start(),
-            vesting.cliff(),
-            vesting.duration(),
-            vesting.beneficiary(),
-            vesting.owner(),
-            vesting.rootChainManager(),
-            vesting.l1Token(),
-            vesting.l2EscrowFactory(),
-            vesting.l2EscrowTemplate(),
-            vesting.historicalBalanceMin()
-        ])
-        const [ start, cliff, duration, beneficiary, owner, vRootChainManager, vL1Token, vL2EscrowFactory, vL2EscrowTemplate, historicalBalanceMin] = results;
-        assert(start.eq(utils.dateToTimestamp(allocations[index].start)),         `Wrong start for vested allocation to ${beneficiary}`);
-        assert(cliff.eq(utils.durationToSeconds(allocations[index].cliff)),       `Wrong cliff for vested allocation to ${beneficiary}`);
-        assert(duration.eq(utils.durationToSeconds(allocations[index].duration)), `Wrong duration for vested allocation to ${beneficiary}`);
-        assert.strictEqual(beneficiary.toLowerCase(), allocations[index].beneficiary.toLowerCase(), `Wrong beneficiary for vested allocation to ${beneficiary}`);
-        assert.strictEqual(owner.toLowerCase(), allocations[index].upgrader.toLowerCase(),    `Wrong admin for direct allocation to ${beneficiary}`);
-        assert.strictEqual(vRootChainManager.toLowerCase(), rootChainManager.toLowerCase(),  `Wrong rootChainManager for ${beneficiary}`);
-        assert.strictEqual(vL1Token.toLowerCase(), l1Token.toLowerCase(),    `Wrong l1Token for direct allocation to ${l1Token}`);
-        assert.strictEqual(vL2EscrowFactory.toLowerCase(), l2EscrowFactory.toLowerCase(),    `Wrong l2EscrowFactory to ${beneficiary}`);
-        assert.strictEqual(vL2EscrowTemplate.toLowerCase(), l2EscrowTemplate.toLowerCase(),    `Wrong l2EscrowFactory to ${beneficiary}`);
-        assert(historicalBalanceMin.eq(ethers.BigNumber.from(0)),       `historicalBalanceMin != 0 ${beneficiary}`);
-        console.log('Checks!...');
+    for(const beneficiary of vestingBeneficiaries) {
+        console.log('Deployed for:', beneficiary)
 
+        const VestingWalletV2 = await ethers.getContractFactory('VestingWalletV2', deployer)
+        const vesting = await VestingWalletV2.deploy(rootChainManager, l1Token, l2EscrowFactory, l2EscrowTemplate)
+        await CACHE_L1.set(`vesting-${beneficiary}.newImpl.address`, vesting.address)
+        console.log('Deployed vesting wallet:', vesting.address);
         index++;
     }
 }
