a3f788905255996c7f67e12302c7d5b0e79c2a91
Raul <raul.mf86@gmail.com>
2022-09-26 18:50:48 +0200

    expand deprecated comment and rename deprecated variable

--- contracts/components/utils/Routed.sol
+++ contracts/components/utils/Routed.sol
@@ -6,16 +6,19 @@ pragma solidity ^0.8.9;
 import "./AccessManaged.sol";
 
 /**
- * Deprecated.
+ * Since Router is deprecated, we are keeping RoutedUpgradeable in this state to preserve storage
+ * layout in deployed `BaseComponentUpgradeable` contracts.
  */
 abstract contract RoutedUpgradeable is AccessManagedUpgradeable {
-    address private _router;
+
+    /// @custom:oz-renamed-from router
+    address private _deprecated_router;
 
     event RouterUpdated(address indexed router);
 
     /// Sets Router instance to address(0). Restricted to DEFAULT_ADMIN_ROLE.
     function disableRouter() public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _router = address(0);
+        _deprecated_router = address(0);
         emit RouterUpdated(address(0));
     }
 
