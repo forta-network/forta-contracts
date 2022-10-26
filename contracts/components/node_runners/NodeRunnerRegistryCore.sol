36296e600c3cdfac168944060852a09af170907d
Raúl <raul.mf86@gmail.com>
2022-10-26 16:50:19 +0200

    Update contracts/components/node_runners/NodeRunnerRegistryCore.sol
    Co-authored-by: Francisco <frangio.1@gmail.com>

--- contracts/components/node_runners/NodeRunnerRegistryCore.sol
+++ contracts/components/node_runners/NodeRunnerRegistryCore.sol
@@ -373,7 +373,6 @@ abstract contract NodeRunnerRegistryCore is BaseComponentUpgradeable, ERC721Upgr
 
     /**
      * Checks if scanner is staked over minimum stake
-     * @param nodeRunnerId scanner
      * @return true if scanner is staked over the minimum threshold for that chainId and is registered,
      * or staking is not yet enabled (stakeController = 0).
      * false otherwise
