0b5ae308fceace989baac88b14a24db99a7317ea
Raul <raul.mf86@gmail.com>
2023-01-27 11:53:08 -0500

    test verify

--- hardhat.config.js
+++ hardhat.config.js
@@ -82,7 +82,7 @@ module.exports = {
     },
 };
 
-const accountsForNetwork = (name) => [argv[`${name}Mnemonic`] && { mnemonic: argv[`${name}Mnemonic`] }, argv[`${name}PrivateKey`] && [argv[`${name}PrivateKey`]]].find(Boolean);
+const accountsForNetwork = (name) => [argv[`${name}Mnemonic`] && { mnemonic: argv[`${name}Mnemonic`] }, argv[`${name}PrivateKey`]].find(Boolean);
 
 Object.assign(
     module.exports.networks,
