2ed66ca47ecc62bd128c3419d09b28a6c2ef4043
Roberto Cantu <Roberto_Cantu@live.com>
2023-05-17 16:36:26 -0500

    Update workflow to upload coverage.json

---
+++ .solcover.js
@@ -0,0 +1,20 @@
+module.exports = {
+    skipFiles: [
+        'components/_old/agents/AgentRegistry_0_1_1.sol',
+        'components/_old/agents/AgentRegistry_0_1_2.sol',
+        'components/_old/agents/AgentRegistry_0_1_4.sol',
+        'components/_old/agents/AgentRegistry_0_1_5.sol',
+        'components/_old/dispatch/Dispatch_0_1_3.sol',
+        'components/_old/dispatch/Dispatch_0_1_4.sol',
+        'components/_old/scanners/ScannerNodeVersion_0_1_0.sol',
+        'components/_old/scanners/ScannerNodeVersion_0_1_1.sol',
+        'components/_old/scanners/ScannerRegistry_0_1_0.sol',
+        'components/_old/scanners/ScannerRegistry_0_1_1.sol',
+        'components/_old/scanners/ScannerRegistry_0_1_2.sol',
+        'components/_old/scanners/ScannerRegistry_0_1_3.sol',
+        'components/_old/staking/FortaStaking_0_1_0.sol',
+        'components/_old/staking/FortaStaking_0_1_1.sol',
+        'components/_old/staking/FortaStakingParameters_0_1_0.sol',
+        'components/_old/staking/FortaStakingParameters_0_1_1.sol'
+    ]
+};
