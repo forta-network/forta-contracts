d4fb80092113745c06fe304f62d21950619598c8
Roberto Cantu <Roberto_Cantu@live.com>
2023-11-28 15:03:54 -0600

    Add modifier logic to handle 'address' as argument

--- contracts/components/threat_oracle/ThreatOracleProvider.sol
+++ contracts/components/threat_oracle/ThreatOracleProvider.sol
@@ -7,42 +7,103 @@ import "./IThreatOracle.sol";
 
 /**
  * Abstract contract to be _inherited_ by a protocol contract that implements the modifiers
- * that will 'consult' the deployed `ThreatOracle` contract and determine whether a given
- * account has been flagged as an exploitative account.
+ * that will 'consult' the deployed `ThreatOracle` contract and determine whether the given
+ * account(s) has/have been flagged as a threat(s).
  * 
- * To properly use the `onlyNonExploitAccount` modifier:
+ * To properly use the provided modifiers:
  * 1. Contract will inherit `ThreatOracleProvider`
- * 2. Contract will pass the address of the deployed `ThreatOracle` in the contractructor
- * 3. Contract will add `onlyNonExploit` to the desired functions for each desired
- *    account (i.e. address). For example:
- *    `onlyNonExploitAccount(msg.sender) onlyNonExploitAccount(tx.origin)` to check
- *    both `msg.sender` and `tx.origin`. Additional ones can be added for arguments
- *    of type `address`.
+ * 2. Contract will pass the follwing in the constructor:
+ *     - address of the deployed `ThreatOracle` by Forta
+ *     - the minimum confidence score a flagged account should exceed to be blocked
+ *     - the maximum amount of addresses a user can pass in an address array
+ *       argument before transaction reverts. Due to it being type `uint8`, its
+ *       natural upper bound is `255`. (DoS prevention)
+ * 3. Contract will add the appropriate modifiers to the desired functions.
+ *    A briefing about each modifier is as follows:
+ *     - `onlyNonThreatMsgSenderAndTxOrigin()`: As implied by the name, this modifier
+ *       checks the msg.sender and tx.origin of each transaction to a specific function.
+ *       This modifier should be added to _all_ functions that require `ThreatOracle`
+ *       services.
+ *     - `onlyNonThreatAccount(address)` is intended to check arguments passed to a
+ *       function of type `address`. If function has multiple arguments of type
+ *       `address`, then modifier can be used more than once. For example:
+ *       ```
+ *          function foobar(
+ *              address _accountOne,
+ *              address _accounTwo
+ *          )
+ *              onlyNonThreatAccount(_accountOne)
+ *              onlyNonThreatAccount(_accounTwo)
+ *          { ... }
+ *       ```
+ *     - `onlyNonThreatAccounts(address[] memory)` is intended to check arguments
+ *     - of type `address[]`. If the function has multiple arguments of this type,
+ *       then the modifier can be used multiple times. For example:
+ *       ```
+ *          function foobar(
+ *              address[] memory _accountsOne,
+ *              address[] calldata _accountsTwo
+ *          )
+ *              onlyNonThreatAccount(_accountOne)
+ *              onlyNonThreatAccount(_accounTwo)
+ *          { ... }
+ *       ```
+ *       Additionally, this modifier implements a upper bound on `address[]` amount
+ *       as a preventative technique for potential DoS attacks
+ *
  */
 abstract contract ThreatOracleProvider {
     bytes32 constant private EXPLOIT_CATEGORY = keccak256("exploit");
     bytes32 constant private MEV_CATEGORY = keccak256("mev");
-    uint8 constant MIN_CONFIDENCESCORE = 90;
 
     IThreatOracle private _threatOracle;
+    uint8 private _minConfidenceScore;
+    // Upper bound of addresses allowed in `address[]` argument
+    // (DoS prevention)
+    uint8 private _maxAddressArgumentAmount;
 
     error ThreatAccountIdentified(address account, string threatCategory, uint8 confidenceScore);
+    error MaxAddressArgumentAmountExceeded(uint8 maxAddressArgumentAmount, uint exceedingAmount);
 
-    modifier onlyNonExploitAccount(address account) {
-        _checkAccount(account);
+    modifier onlyNonThreatMsgSenderAndTxOrigin() {
+        _confirmNonThreatAccount(msg.sender);
+        _confirmNonThreatAccount(tx.origin);
         _;
     }
 
-    constructor(address __threatOracle) {
-        _threatOracle = IThreatOracle(__threatOracle);
+    modifier onlyNonThreatAccount(address account) {
+        _confirmNonThreatAccount(account);
+        _;
     }
 
-    function _checkAccount(address account) internal view {
+    modifier onlyNonThreatAccounts(address[] memory accounts) {
+        uint256 accountsAmount = accounts.length;
+        if (accountsAmount > _maxAddressArgumentAmount) {
+            revert MaxAddressArgumentAmountExceeded(_maxAddressArgumentAmount, accountsAmount);
+        }
+
+        for (uint256 i = 0; i < accountsAmount;) {
+            _confirmNonThreatAccount(accounts[i]);
+
+            unchecked {
+                ++i;
+            }
+        }
+        _;
+    }
+
+    constructor(address __threatOracle, uint8 __minConfidenceScore, uint8 __maxAddressArgumentAmount) {
+        _threatOracle = IThreatOracle(__threatOracle);
+        _minConfidenceScore = __minConfidenceScore;
+        _maxAddressArgumentAmount = __maxAddressArgumentAmount;
+    }
+
+    function _confirmNonThreatAccount(address account) internal view {
         (string memory category, uint8 confidenceScore) = _threatOracle.getThreatProperties(account);
         bytes32 categoryHashed = keccak256(abi.encodePacked(category));
 
         if ((categoryHashed == EXPLOIT_CATEGORY || categoryHashed == MEV_CATEGORY) && 
-            confidenceScore >= MIN_CONFIDENCESCORE
+            confidenceScore >= _minConfidenceScore
         ) revert ThreatAccountIdentified(account, category, confidenceScore);
     }
 }
--- contracts/mocks/threat_oracle/MockThreatOracleConsumer.sol
+++ contracts/mocks/threat_oracle/MockThreatOracleConsumer.sol
@@ -7,20 +7,44 @@ import "../../components/threat_oracle/ThreatOracleProvider.sol";
 
 contract MockThreatOracleConsumer is ThreatOracleProvider {
 
-    constructor(address __threatOracle) ThreatOracleProvider(__threatOracle) {}
+    constructor(address _threatOracle, uint8 _minConfidenceScore, uint8 __maxAddressArgumentAmount)
+        ThreatOracleProvider(_threatOracle, _minConfidenceScore, __maxAddressArgumentAmount)
+    {}
 
-    function foo() public view onlyNonExploitAccount(msg.sender) onlyNonExploitAccount(tx.origin) returns (bool) {
+    function foo() public view onlyNonThreatMsgSenderAndTxOrigin() returns (bool) {
         return true;
     }
 
     function bar(address _account)
         public
         view
-        onlyNonExploitAccount(msg.sender)
-        onlyNonExploitAccount(tx.origin)
-        onlyNonExploitAccount(_account)
+        onlyNonThreatMsgSenderAndTxOrigin()
+        onlyNonThreatAccount(_account)
         returns (bool)
     {
         return true;
     }
+
+    function foobar(address _accountOne, address _accounTwo)
+        public
+        view
+        onlyNonThreatMsgSenderAndTxOrigin()
+        onlyNonThreatAccount(_accountOne)
+        onlyNonThreatAccount(_accounTwo)
+        returns (bool)
+    {
+        return true;
+    }
+
+    function foobarTwo(address[] memory _accountsOne, address[] calldata _accountsTwo)
+        public
+        view
+        onlyNonThreatMsgSenderAndTxOrigin()
+        onlyNonThreatAccounts(_accountsOne)
+        onlyNonThreatAccounts(_accountsTwo)
+        returns (bool)
+    {
+        return true;
+    }
+
 }
