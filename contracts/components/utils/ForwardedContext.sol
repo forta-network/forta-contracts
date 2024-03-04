c02b4b92ebd943aa3bef1fab5c3e61fd1ed45241
Roberto Cantu <Roberto_Cantu@live.com>
2024-03-04 15:04:01 -0600

    Remove trustedForwarder usage

--- contracts/components/utils/ForwardedContext.sol
+++ contracts/components/utils/ForwardedContext.sol
@@ -10,8 +10,6 @@ abstract contract ForwardedContext is ContextUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
     address private immutable _trustedForwarder;
 
-    uint256 private constant ADDRESS_SIZE_BYTES = 20;
-
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor(address trustedForwarder) {
         // WARNING: do not set this address to other than a deployed Forwarder instance.
@@ -20,36 +18,19 @@ abstract contract ForwardedContext is ContextUpgradeable {
         _trustedForwarder = trustedForwarder;
     }
 
-    /// Checks if `forwarder` address provided is the trustedForwarder set in the constructor.
-    function isTrustedForwarder(address forwarder) public view virtual returns (bool) {
-        return forwarder == _trustedForwarder;
-    }
-
     /**
-     * @notice Gets sender of the transaction of signer if meta transaction.
-     * @dev If the tx is sent by the trusted forwarded, we assume it is a meta transaction and 
-     * the signer address is encoded in the last 20 bytes of msg.data.
+     * @notice Gets sender of the transaction.
      * @return sender address of sender of the transaction of signer if meta transaction.
      */
     function _msgSender() internal view virtual override returns (address sender) {
-        if (isTrustedForwarder(msg.sender)) {
-            return address(bytes20(msg.data[msg.data.length - ADDRESS_SIZE_BYTES: msg.data.length]));
-        } else {
-            return super._msgSender();
-        }
+        return super._msgSender();
     }
 
     /**
-     * @notice Gets msg.data of the transaction or meta-tx.
-     * @dev If the tx is sent by the trusted forwarded, we assume it is a meta transaction and 
-     * msg.data must have the signer address (encoded in the last 20 bytes of msg.data) removed.
-     * @return msg.data of the transaction of msg.data - signer address if meta transaction.
+     * @notice Gets msg.data of the transaction.
+     * @return msg.data of the transaction of msg.data.
      */
     function _msgData() internal view virtual override returns (bytes calldata) {
-        if (isTrustedForwarder(msg.sender)) {
-            return msg.data[:msg.data.length - ADDRESS_SIZE_BYTES];
-        } else {
-            return super._msgData();
-        }
+        return super._msgData();
     }
 }
